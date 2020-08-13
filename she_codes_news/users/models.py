ffc0f9b2fa52b355dc8f4c3cc64c850a3725c875
C2jay <cat.crane15@gmail.com>
2020-08-13 10:07:15 +0800

    profile image fix

--- she_codes_news/users/models.py
+++ she_codes_news/users/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class CustomUser(AbstractUser):
-    profileimg = models.ImageField(upload_to='images', default='img.jpg')
+    profileimg = models.ImageField(upload_to='media', default='img.jpg')
     bio = models.TextField(max_length=1000, default="There is no bio yet!")
 
     def __str__(self):
