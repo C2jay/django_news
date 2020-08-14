09bf400d76d4e5d020a0d8caf68460fac3a7e6cc
C2jay <cat.crane15@gmail.com>
2020-08-14 12:03:25 +0800

    homepage css edit

--- she_codes_news/news/urls.py
+++ she_codes_news/news/urls.py
@@ -5,7 +5,7 @@ app_name = 'news'
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('<slug:slug>/', views.StoryView.as_view(), name='story'),
+    path('story/<str:slug>/', views.StoryView.as_view(), name='story'),
     path('add-story/', views.AddStoryView.as_view(), name='newsStory'),
     path('<int:pk>/edit-story/', views.UpdateStoryView.as_view(), name='editStory'),
     path('<int:pk>/delete-story/', views.DeleteStoryView.as_view(), name='deleteStory'),
