e7f2c9792c8ae1b5372cb701fb2ed752d510cb29
C2jay <cat.crane15@gmail.com>
2020-08-14 13:36:56 +0800

    update date order

--- she_codes_news/news/views.py
+++ she_codes_news/news/views.py
@@ -58,8 +58,8 @@ class IndexView(generic.ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['latest_stories'] = NewsStory.objects.all().order_by('pub_date')[:4]
-        context['all_stories'] = NewsStory.objects.all().order_by('pub_date')[4:]
+        context['latest_stories'] = NewsStory.objects.all().order_by('-pub_date')[:4]
+        context['all_stories'] = NewsStory.objects.all().order_by('-pub_date')[4:]
         return context
 
 class StoryView(generic.DetailView):
@@ -73,7 +73,7 @@ class UpdateStoryView(LoginRequiredMixin, UserPassesTestMixin, generic.UpdateVie
     context_object_name = 'storyForm'
     model = NewsStory
     template_name = 'news/updateStory.html'
-    
+
     def get_success_url(self):
         """ get the redirect prior to delete"""
         author_id = self.get_object().author.pk
